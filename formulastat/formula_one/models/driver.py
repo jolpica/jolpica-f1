2404aeb36e4486181a47f74f85fca0fc88e67efa
jolpica <47005135+jolpica@users.noreply.github.com>
2023-10-14 17:09:50 +0000

    add formula_one app

--- formulastat/formula_one/models/driver.py
+++ formulastat/formula_one/models/driver.py
@@ -2,12 +2,29 @@ from django.db import models
 
 
 class Driver(models.Model):
+    """
+    Table driver {
+      id integer [primary key]
+      reference string [unique]
+      forename string
+      surname string
+      abbreviation string
+      nationality string
+      permanent_car_number integer
+      date_of_birth date
+      wikipedia string
+    }
+    """
+
     id = models.BigAutoField(primary_key=True)
     reference = models.CharField(max_length=32, unique=True, null=True, blank=True)
     forename = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
     abbreviation = models.CharField(max_length=10, null=True, blank=True)
+    nationality = models.CharField(max_length=255, null=True, blank=True)
     permanent_car_number = models.PositiveSmallIntegerField(null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
     wikipedia = models.URLField(max_length=255, null=True, blank=True)
 
+    def __str__(self) -> str:
+        return f"{self.forename} {self.surname}"
---
+++ formulastat/formula_one/models/race.py
@@ -0,0 +1,56 @@
+from typing import ClassVar
+
+from django.db import models
+
+
+class Race(models.Model):
+    """
+    Table race {
+        id integer [primary key]
+        season_id integer
+        circuit_id integer
+        round integer
+        name varchar
+        date date
+        wikipedia string
+        cancelled bool
+    }
+    """
+
+    id = models.BigAutoField(primary_key=True)
+    season = models.ForeignKey("Season", on_delete=models.CASCADE, related_name="races")
+    round = models.PositiveSmallIntegerField(null=True, blank=True)
+    circuit = models.ForeignKey("Circuit", on_delete=models.CASCADE, related_name="races")
+    name = models.CharField(max_length=255, null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
+    wikipedia = models.URLField(null=True, blank=True)
+    is_cancelled = models.BooleanField(default=False)
+
+    class Meta:
+        constraints: ClassVar = [models.UniqueConstraint(fields=["season", "round"], name="race_unique_season_round")]
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class RaceEntry(models.Model):
+    """
+    Table race_entry {
+        id integer [primary key]
+        driver_id integer
+        race_id integer
+        team_id integer
+        Note: 'Driver data per race'
+    }
+    """
+
+    id = models.BigAutoField(primary_key=True)
+    race = models.ForeignKey("Race", on_delete=models.CASCADE, related_name="race_entries")
+    driver = models.ForeignKey("Driver", on_delete=models.CASCADE, related_name="race_entries")
+    team = models.ForeignKey("Team", on_delete=models.CASCADE, related_name="race_entries")
+
+    class Meta:
+        constraints: ClassVar = [models.UniqueConstraint(fields=["race", "driver", "team"], name="race_entry_unique")]
+
+    def __str__(self) -> str:
+        return f"{self.driver}, {self.team} - {self.race}"
