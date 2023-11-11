6302c0e406951d99057d627677e57ae0cce02abd
jperry <jorge.perry@skyscanner.net>
2023-11-11 19:56:43 +0000

    add remaining criteria filters for season

--- formulastat/ergastapi/views.py
+++ formulastat/ergastapi/views.py
@@ -4,6 +4,7 @@ from rest_framework import permissions, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from formulastat.ergast.models import Status
 from formulastat.formula_one.models import Season, SessionType
 
 from . import pagination, serializers
@@ -14,22 +15,39 @@ class SeasonViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
     pagination_class = pagination.ErgastAPIPagination
 
-    def get_queryset(self) -> QuerySet:
-        queryset = Season.objects.all()
+    def get_criteria_filters(
+        self,
+        circuit_ref=None,
+        team_ref=None,
+        driver_ref=None,
+        grid_position=None,
+        race_position=None,
+        ergast_status_id=None,
+        **kwargs,
+    ) -> Q:
         filters = Q()
-        if self.kwargs.get("circuit_ref"):
-            filters = filters & Q(races__circuit__reference=self.kwargs["circuit_ref"])
-        if self.kwargs.get("team_ref"):
-            filters = filters & Q(team_drivers__team__reference=self.kwargs["team_ref"])
-        if self.kwargs.get("driver_ref"):
-            filters = filters & Q(team_drivers__driver__reference=self.kwargs["driver_ref"])
-        if self.kwargs.get("grid_position"):
-            filters = (
-                filters
-                & Q(team_drivers__race_entries__session_entries__grid=self.kwargs["grid_position"])
-                & Q(team_drivers__race_entries__session_entries__session__type=SessionType.RACE)
+        if circuit_ref:
+            # filters = filters & Q(races__circuit__reference=circuit_ref)
+            filters = filters & Q(team_drivers__race_entries__race__circuit__reference=circuit_ref)
+        if team_ref:
+            filters = filters & Q(team_drivers__team__reference=team_ref)
+        if driver_ref:
+            filters = filters & Q(team_drivers__driver__reference=driver_ref)
+        if grid_position or race_position:
+            filters = filters & Q(team_drivers__race_entries__session_entries__session__type=SessionType.RACE)
+            if grid_position:
+                filters = filters & Q(team_drivers__race_entries__session_entries__grid=grid_position)
+            if race_position:
+                filters = filters & Q(team_drivers__race_entries__session_entries__position=race_position)
+        if ergast_status_id:
+            filters = filters & Q(
+                team_drivers__race_entries__session_entries__detail=Status.objects.get(pk=ergast_status_id)
             )
-        return queryset.filter(filters).order_by("year").distinct()
+        return filters
+
+    def get_queryset(self) -> QuerySet:
+        filters = self.get_criteria_filters(**self.kwargs)
+        return Season.objects.filter(filters).order_by("year").distinct()
 
     @action(methods=["get"], detail=False)
     def drivers(self, request, circuit_ref=None, constructor_ref=None, driver_ref=None):
