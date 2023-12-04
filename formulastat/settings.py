33c1e91ec720526792538fd616cfeff307673d5b
jolpica <47005135+jolpica@users.noreply.github.com>
2023-12-04 10:32:56 +0000

    add static file generation

--- formulastat/settings.py
+++ formulastat/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
