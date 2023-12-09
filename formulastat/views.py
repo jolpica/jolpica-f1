59be1e6ed532ecb3a36f45b08f867eaa65270a62
jolpica <47005135@users.noreply.github.com>
2023-12-09 15:23:48 +0000

    simplify healthcheck

--- formulastat/views.py
+++ formulastat/views.py
@@ -1,7 +1,5 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.http import JsonResponse
 
 
-@api_view(["GET"])
 def healthcheck(request):
-    return Response(status=200)
+    return JsonResponse(data={"status": "OK"}, status=200)
