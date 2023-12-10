6f26b7819a66bf6ac608899ce7586e0b0384393a
jolpica <47005135@users.noreply.github.com>
2023-12-10 14:52:18 +0000

    remove broken is_ec2_linux check

--- formulastat/deployment_utils.py
+++ formulastat/deployment_utils.py
@@ -1,17 +1,4 @@
-import os
-
 import requests
-
-
-def is_ec2_linux() -> bool:
-    """Detect if we are running on an EC2 Linux Instance
-    See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
-    """
-    if os.path.isfile("/sys/hypervisor/uuid"):
-        with open("/sys/hypervisor/uuid") as f:
-            uuid = f.read()
-            return uuid.startswith("ec2")
-    return False
 
 
 def get_ec2_token() -> str:
@@ -23,24 +10,16 @@ def get_ec2_token() -> str:
     return response.text
 
 
-def get_linux_ec2_private_ip() -> None | list[str]:
+def get_linux_ec2_private_ip() -> None | str:
     """Get the private IP Address of the machine if running on an EC2 linux server.
     See https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
     """
 
-    if not is_ec2_linux():
-        return None
     try:
         token = get_ec2_token()
-        headers = {
-            "X-aws-ec2-metadata-token": f"{token}",
-        }
-        ips = []
+        headers = {"X-aws-ec2-metadata-token": f"{token}"}
         response = requests.get("http://169.254.169.254/latest/meta-data/local-ipv4", headers=headers, timeout=60)
-        ips.append(response.text)
-        response = requests.get("http://169.254.169.254/latest/meta-data/public-ip", headers=headers, timeout=60)
-        ips.append(response.text)
-        return ips
+        return response.text
     except Exception:
         return None
     finally:
