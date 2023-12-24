39feb1a7a8fc811533f2c5b78187f67565410ac0
jolpica <47005135@users.noreply.github.com>
2023-12-24 23:39:20 +0000

    refactor calculate championship points

--- formulastat/formula_one/tests/test_utils.py
+++ formulastat/formula_one/tests/test_utils.py
@@ -1,6 +1,37 @@
 import pytest
 
-from ..utils import races_to_championship_points
+from ..models import BestResultsPerSplitScheme, SeasonSplitScheme
+from ..utils import calculate_championship_points
+
+
+def year_to_scheme(year: int | str) -> tuple[int, int, int | None]:
+    """Return (split scheme, best results, total_rounds) for a given year"""
+    match year:
+        case 2023:
+            return (0, -1, 22)
+        case 1991:
+            return (0, -1, 99)
+        case 1990:
+            return (0, 11, 16)
+        case 1985:
+            return (0, 11, 99)
+        case 1980:
+            return (3, 5, 14)
+        case 1979:
+            return (3, 4, 15)
+        case 1978:
+            return (2, -2, 16)
+        case 1977:
+            return (2, -2, 17)
+        case 1966:
+            return (0, 5, 99)
+        case 1958:
+            return (0, 6, 99)
+        case "c1951":
+            return (0, 0, 9)
+        case "c1950":
+            return (0, 0, 99)
+    raise NotImplementedError()
 
 
 @pytest.mark.parametrize(
@@ -12,9 +43,9 @@ from ..utils import races_to_championship_points
         (2023, {1: 25.0, 2: 1, 5: None, 17: 12.5}, 38.5),
         (1991, {22: 0.0}, 0),
         # 1981 - 1990 Best 11 races
-        (1985, {}, 0),
         (1990, {1: 1, 2: 5}, 6),
-        (1981, {(i + 1): i % 2 + 1 for i in range(20)}, 21),  # 20 alternating scores of 1 and 2
+        (1985, {}, 0),
+        (1985, {(i + 1): i % 2 + 1 for i in range(20)}, 21),  # 20 alternating scores of 1 and 2
         # 1980 Best 5 from both first and last 7
         (1980, {}, 0),
         (1980, {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 5, 7: 1, 8: 1}, 10),
@@ -28,48 +59,46 @@ from ..utils import races_to_championship_points
         # 1967 - 1978, best n-1 of each half of season
         # 1978, 1976 16 rounds
         (1978, {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 5, 7: 1, 8: 1}, 11),
-        (1976, {1: 1, 2: 1, 3: 1, 6: 5, 7: 1, 8: 1, 11: 5}, 15),
-        (1976, {i + 1: 1 for i in range(16)}, 14),
+        (1978, {1: 1, 2: 1, 3: 1, 6: 5, 7: 1, 8: 1, 11: 5}, 15),
+        (1978, {i + 1: 1 for i in range(16)}, 14),
         # 1977 17 rounds
-        (1977, {i + 1: 1 for i in range(9)}, 8),  
-        (1977, {i + 1: 1 for i in range(10)}, 9),  
+        (1977, {i + 1: 1 for i in range(9)}, 8),
+        (1977, {i + 1: 1 for i in range(10)}, 9),
         (1977, {i + 1: 1 for i in range(17)}, 15),
         # 1966 Best 5
-        (1966, {1: 0, 2: 3,	3:9,4: 9, 5: 9, 6: 9, 7:0,	8:0,9:6}, 42),
+        (1966, {1: 0, 2: 3, 3: 9, 4: 9, 5: 9, 6: 9, 7: 0, 8: 0, 9: 6}, 42),
         # 1958 Best 6
-        (1958, {1: 4, 3:2, 5:7, 7:7, 9:7,10:6,11:6, 6:9}, 42),
+        (1958, {1: 4, 3: 2, 5: 7, 7: 7, 9: 7, 10: 6, 11: 6, 6: 9}, 42),
+        # Constructor championship before 1958 (not available)
+        ("c1951", {i + 1: 1 for i in range(15)}, None),
+        ("c1950", {}, None),
     ),
 )
-def test_races_to_championship_points(year, race_points, expected):
-    assert races_to_championship_points(year, race_points) == expected
+def test_calculate_championship_points(year, race_points, expected):
+    assert calculate_championship_points(race_points, *year_to_scheme(year)) == expected
+
 
 @pytest.mark.parametrize(
-    ["year", "race_points", "expected"],
+    ["race_points", "split", "results", "total_rounds"],
     (
-        (1978, {i + 1: 1 for i in range(16)}, 14),
-        (1979, {i + 1: 1 for i in range(15)}, 15),
-        (1980, {i + 1: 1 for i in range(15)}, 15),
-        (1985, {i + 1: 1 for i in range(15)}, 15),
+        # Invalid round numbers
+        ({0: 5}, 0, 5, 9),
+        ({-5: 1.0}, 0, 5, 9),
+        # no total rounds for split schema
+        ({1: 1}, 1, 5, 99),
+        # Invalid split type
+        ({1: 1}, 99, 5, 99),
+        # total rounds less than or equal to 0
+        ({1: 1}, 1, 5, 0),
     ),
 )
-def test_races_to_championship_points_constructors(year, race_points, expected):
-    assert races_to_championship_points(year, race_points, True) == expected
-
-@pytest.mark.parametrize(
-    ["year", "race_points", "is_constructors"],
-    (
-        (2023, {0: 5}, False),
-        (1950, {-5: 1.0}, False),
-        (1949, {1: 1}, False),
-        (1957, {1: 1}, True),
-    ),
-)
-def test_races_to_championship_points_error(year, race_points, is_constructors):
+def test_calculate_championship_points_error(race_points, split, results, total_rounds):
     with pytest.raises(ValueError):
-        races_to_championship_points(year, race_points, is_constructors)
+        calculate_championship_points(race_points, split, results, total_rounds)
 
-def test_races_to_championship_points_all_years():
-    for year in range(1950, 2030):
-        assert races_to_championship_points(year, {1: 4, 6: 6}) == 10
-    for year in range(1958, 2030):
-        assert races_to_championship_points(year, {1: 4, 6: 6}, True) == 10
+
+def test_calculate_championship_points_all_variations():
+    for split in SeasonSplitScheme:
+        for best_results in BestResultsPerSplitScheme:
+            for total_rounds in [1, 17]:
+                assert calculate_championship_points({1: 4, 9: 6}, split, best_results, total_rounds) in (None, 10, 0)
