36e48907ce735ded94714378402c97e7b008f132
jolpica <47005135+jolpica@users.noreply.github.com>
2024-01-29 00:07:59 +0000

    add team championship

--- jolpica/formula_one/models/managed_views/championship_standing.py
+++ jolpica/formula_one/models/managed_views/championship_standing.py
@@ -8,8 +8,8 @@ from ..point_scheme import ChampionshipAdjustmentType
 class DriverChampionship(models.Model):
     id = models.BigAutoField(primary_key=True)
 
-    session = models.ForeignKey("formula_one.Session", on_delete=models.CASCADE, related_name="driver_standings")
-    driver = models.ForeignKey("formula_one.Driver", on_delete=models.CASCADE, related_name="driver_standings")
+    session = models.ForeignKey("formula_one.Session", on_delete=models.CASCADE, related_name="driver_championships")
+    driver = models.ForeignKey("formula_one.Driver", on_delete=models.CASCADE, related_name="driver_championships")
     year = models.SmallIntegerField()
     round_number = models.PositiveSmallIntegerField()
     position = models.SmallIntegerField(null=True)
@@ -22,15 +22,47 @@ class DriverChampionship(models.Model):
     adjustment_type = models.PositiveSmallIntegerField(choices=ChampionshipAdjustmentType.choices, default=0)
 
     season = models.ForeignKey(
-        "formula_one.Season", null=True, blank=True, on_delete=models.SET_NULL, related_name="driver_standings"
+        "formula_one.Season", null=True, blank=True, on_delete=models.SET_NULL, related_name="driver_championships"
     )
     round = models.ForeignKey(
-        "formula_one.Round", null=True, blank=True, on_delete=models.SET_NULL, related_name="driver_standings"
+        "formula_one.Round", null=True, blank=True, on_delete=models.SET_NULL, related_name="driver_championships"
     )
 
     class Meta:
         constraints: ClassVar = [
-            models.UniqueConstraint(fields=["session", "driver"], name="driver_standing_unique_session_driver"),
+            models.UniqueConstraint(fields=["session", "driver"], name="driver_championship_unique_session_driver"),
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.year}"
+
+
+class TeamChampionship(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+    session = models.ForeignKey("formula_one.Session", on_delete=models.CASCADE, related_name="team_championships")
+    team = models.ForeignKey("formula_one.Team", on_delete=models.CASCADE, related_name="team_championships")
+    year = models.SmallIntegerField()
+    round_number = models.PositiveSmallIntegerField()
+    position = models.SmallIntegerField(null=True)
+    points = models.FloatField()
+    win_count = models.SmallIntegerField()
+    highest_finish = models.SmallIntegerField(null=True)
+    finish_string = models.CharField(max_length=255)
+
+    is_eligible = models.BooleanField(default=False)
+    adjustment_type = models.PositiveSmallIntegerField(choices=ChampionshipAdjustmentType.choices, default=0)
+
+    season = models.ForeignKey(
+        "formula_one.Season", null=True, blank=True, on_delete=models.SET_NULL, related_name="team_championships"
+    )
+    round = models.ForeignKey(
+        "formula_one.Round", null=True, blank=True, on_delete=models.SET_NULL, related_name="team_championships"
+    )
+
+    class Meta:
+        constraints: ClassVar = [
+            models.UniqueConstraint(fields=["session", "team"], name="team_championship_unique_session_team"),
         ]
 
     def __str__(self) -> str:
