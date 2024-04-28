c8e1bc9cf8e32807f8ba8f328690dbd656eec0eb
JorVLP <47005135+JorVLP@users.noreply.github.com>
2024-04-28 20:08:45 +0000

    install django-ratelimit

--- jolpica_api/views.py
+++ jolpica_api/views.py
@@ -2,12 +2,19 @@ from datetime import datetime
 
 from django.http import JsonResponse
 from django.http.request import HttpRequest
+from django_ratelimit.decorators import ratelimit
 
 
 def healthcheck(request: HttpRequest):
     return JsonResponse(data={"status": "OK"}, status=200)
 
 
+def ratelimited_error(request: HttpRequest, exception):
+    return JsonResponse(data={"error": "ratelimited"}, status=429)
+
+
+@ratelimit(group="test", key="user_or_ip", rate="1/s", block=False)
+@ratelimit(group="test", key="user_or_ip", rate="10/m", block=True)
 def test(request: HttpRequest):
     try:
         return JsonResponse(
@@ -15,6 +22,7 @@ def test(request: HttpRequest):
                 "timestamp": datetime.now(),
                 "REMOTE_ADDR": request.META["REMOTE_ADDR"],
                 "HTTP_X_FORWARDED_FOR": request.META.get("HTTP_X_FORWARDED_FOR", ""),
+                "ratelimited": getattr(request, "limited", False),
             },
             status=200,
         )
