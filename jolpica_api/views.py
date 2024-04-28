8f18a327d1efe449ae9d9ca1c9413e45fe856a3d
jolpica <47005135@users.noreply.github.com>
2024-04-28 19:10:06 +0000

    Add test endpoint

--- jolpica_api/views.py
+++ jolpica_api/views.py
@@ -1,5 +1,22 @@
+from datetime import datetime
+
 from django.http import JsonResponse
+from django.http.request import HttpRequest
 
 
-def healthcheck(request):
+def healthcheck(request: HttpRequest):
     return JsonResponse(data={"status": "OK"}, status=200)
+
+
+def test(request: HttpRequest):
+    try:
+        return JsonResponse(
+            data={
+                "timestamp": datetime.now(),
+                "REMOTE_ADDR": request.META["REMOTE_ADDR"],
+                "HTTP_X_FORWARDED_FOR": request.META.get("HTTP_X_FORWARDED_FOR", ""),
+            },
+            status=200,
+        )
+    except Exception as ex:
+        return JsonResponse({"error": repr(ex)})
