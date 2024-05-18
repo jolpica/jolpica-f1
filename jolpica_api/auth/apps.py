ed48bf7edb739d65019f42dced83c79576b53139
JorVLP <47005135+JorVLP@users.noreply.github.com>
2024-05-18 21:40:51 +0000

    custom label for auth app

--- jolpica_api/auth/apps.py
+++ jolpica_api/auth/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class AuthConfig(AppConfig):
+class ApiAuthConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "jolpica_api.auth"
+    label = "jolpica_api_auth"
