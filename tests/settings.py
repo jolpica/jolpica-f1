91fb17d7c30fe430821dce872fa02c9efaf8bed0
jolpica <47005135+jolpica@users.noreply.github.com>
2024-05-18 21:20:43 +0000

    implement token based authentication

--- tests/settings.py
+++ tests/settings.py
@@ -1,3 +1,10 @@
 from jolpica_api.settings import *  # noqa: F403
 
 INSTALLED_APPS += ["tests"]  # noqa: F405
+
+REST_KNOX.update(
+    {
+        # Use faster hash algorithm for testing
+        "SECURE_HASH_ALGORITHM": "hashlib.md5",
+    }
+)
