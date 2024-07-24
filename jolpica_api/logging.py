606bc6f8aa09c159066c3e5723f72da220b11a40
jolpica <47005135+jolpica@users.noreply.github.com>
2024-07-24 20:24:15 +0000

    Fix Invalid type ASGIRequest error message

--- jolpica_api/logging.py
+++ jolpica_api/logging.py
@@ -1,9 +1,10 @@
 import logging
 import logging.config
 
+from django.http.request import HttpRequest
 from gunicorn import glogging
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
-from opentelemetry.sdk._logs import LoggerProvider
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 
 # Authenticates with the OTEL_EXPORTER_OTLP_HEADERS environment variable
@@ -11,6 +12,23 @@ from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 logger_provider = LoggerProvider()
 exporter = OTLPLogExporter()
 logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
+
+
+class CustomLoggingHandler(LoggingHandler):
+    @staticmethod
+    def _get_attributes(record: logging.LogRecord):
+        attributes = LoggingHandler._get_attributes(record)
+        if isinstance(attributes, dict):
+            if isinstance(attributes.get("request"), HttpRequest):
+                # opentelemetry.attributes.BoundedAttributes does not support HttpRequest
+                # Split request into compatible types if present
+                # This occurs when getting "Not Found" error
+                request: HttpRequest = attributes.pop("request")
+                attributes["request.path"] = request.path
+                attributes["request.method"] = request.method
+
+        return attributes
+
 
 LOG_CONFIG = {
     "version": 1,
@@ -21,7 +39,7 @@ LOG_CONFIG = {
     "handlers": {
         "console": {"level": "DEBUG", "class": "logging.StreamHandler", "formatter": "standard"},
         "otlp": {
-            "class": "opentelemetry.sdk._logs.LoggingHandler",
+            "class": "jolpica_api.logging.CustomLoggingHandler",
             "logger_provider": logger_provider,
         },
     },
