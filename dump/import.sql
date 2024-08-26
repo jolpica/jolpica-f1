c6901738291914e6d8043f0fbc1669726499a640
jolpica <47005135+jolpica@users.noreply.github.com>
2024-08-26 18:44:37 +0000

    New fixtures

---
+++ dump/import.sql
@@ -0,0 +1,4 @@
+\copy formula_one_roundentry FROM 'dump/roundentrys.csv' WITH CSV HEADER;
+\copy formula_one_sessionentry FROM 'dump/sessionentrys.csv' WITH CSV HEADER;
+\copy formula_one_lap FROM 'dump/laps.csv' WITH CSV HEADER;
+\copy formula_one_pitstop FROM 'dump/pitstops.csv' WITH CSV HEADER;
