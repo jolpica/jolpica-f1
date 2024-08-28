6e9abbee874dcb5035280595b0fdee9d5cf1dd73
jolpica <47005135+jolpica@users.noreply.github.com>
2024-08-28 22:02:07 +0000

    refactor test fixtures to reduce size

---
+++ tests/create_data_fixtures.sh
@@ -0,0 +1,78 @@
+# team champ 1,376
+# driver champ 3,280
+
+# lap 44,000
+# sessionentry 4,800
+# roundentry 1,600
+# pitstop 1,040
+
+
+pg_dump -h localhost -U postgres -d jolpica  -a \
+    -t "formula_one*" \
+    -t "ergast_status" \
+    -T "formula_one_lap" \
+    -T "formula_one_*championship" \
+    -T "formula_one_pitstop" \
+    -T "formula_one_*entry" > tests/fixtures/db/tables.sql
+
+SESSION_ENTRY_WHERE_CLAUSE="1979,1980,1997,2007,2008,2009,2020,2023"
+PITSTOP_WHERE_CLAUSE="season.year = 2023 AND round.number in (1, 10, 18)"
+psql -h localhost -U postgres -d jolpica -c "\copy
+(
+SELECT pitstop.*
+FROM 
+    formula_one_pitstop as pitstop 
+    JOIN formula_one_lap as lap ON pitstop.lap_id = lap.id
+    JOIN formula_one_sessionentry as sessionentry ON pitstop.session_entry_id = sessionentry.id 
+    JOIN formula_one_session as session ON sessionentry.session_id = session.id
+    JOIN formula_one_round as round ON session.round_id = round.id
+    JOIN formula_one_season as season ON round.season_id = season.id
+WHERE 
+    $PITSTOP_WHERE_CLAUSE
+) to 'tests/fixtures/db/pitstops.csv' 
+WITH CSV HEADER;"
+
+LAP_WHERE_CLAUSE="
+    (season.year = 2023 AND (round.number in (1, 10, 18)))
+    OR ($PITSTOP_WHERE_CLAUSE)
+"
+psql -h localhost -U postgres -d jolpica -c "\copy
+(
+SELECT lap.*
+FROM 
+    formula_one_lap as lap 
+    JOIN formula_one_sessionentry as sessionentry ON lap.session_entry_id = sessionentry.id 
+    JOIN formula_one_session as session ON sessionentry.session_id = session.id
+    JOIN formula_one_round as round ON session.round_id = round.id
+    JOIN formula_one_season as season ON round.season_id = season.id
+WHERE 
+    (lap.is_entry_fastest_lap = true and (season.year in ($SESSION_ENTRY_WHERE_CLAUSE)))
+    OR ($LAP_WHERE_CLAUSE)
+) to 'tests/fixtures/db/laps.csv'
+WITH CSV HEADER;"
+
+psql -h localhost -U postgres -d jolpica -c "\copy
+(
+SELECT sessionentry.*
+FROM 
+    formula_one_sessionentry as sessionentry
+    JOIN formula_one_session as session ON sessionentry.session_id = session.id
+    JOIN formula_one_round as round ON session.round_id = round.id
+    JOIN formula_one_season as season ON round.season_id = season.id
+WHERE 
+    (season.year IN ($SESSION_ENTRY_WHERE_CLAUSE))
+    OR ($LAP_WHERE_CLAUSE)
+) to 'tests/fixtures/db/sessionentrys.csv'
+WITH CSV HEADER;"
+
+psql -h localhost -U postgres -d jolpica -c "\copy (
+SELECT roundentry.*
+FROM 
+    formula_one_roundentry as roundentry
+    JOIN formula_one_round as round ON roundentry.round_id = round.id
+    JOIN formula_one_season as season ON round.season_id = season.id
+WHERE 
+    (season.year IN ($SESSION_ENTRY_WHERE_CLAUSE))
+    OR ($LAP_WHERE_CLAUSE)
+) TO 'tests/fixtures/db/roundentrys.csv'
+WITH CSV HEADER;"
---
+++ tests/fixtures/import.sql
@@ -0,0 +1,5 @@
+\copy formula_one_roundentry FROM 'tests/fixtures/db/roundentrys.csv' WITH CSV HEADER;
+\copy formula_one_sessionentry FROM 'tests/fixtures/db/sessionentrys.csv' WITH CSV HEADER;
+\copy formula_one_lap FROM 'tests/fixtures/db/laps.csv' WITH CSV HEADER;
+\copy formula_one_pitstop FROM 'tests/fixtures/db/pitstops.csv' WITH CSV HEADER;
+SELECT * from formula_one_roundentry;
