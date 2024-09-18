b8651790c9db527d58043f4f5bc7a56f3af51fd3
jolpica <47005135+jolpica@users.noreply.github.com>
2024-09-18 23:32:52 +0000

    Filter out invalid host logs

--- jolpica_api/logging.py
+++ jolpica_api/logging.py
@@ -1,6 +1,7 @@
 import logging
 import logging.config
 
+from django.core.exceptions import SuspiciousOperation
 from django.http.request import HttpRequest
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
@@ -14,6 +15,13 @@ logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
 
 
 class CustomLoggingHandler(LoggingHandler):
+    def filter(self, record: logging.LogRecord) -> bool:
+        if record.exc_info:
+            exc_value = record.exc_info[1]
+            if isinstance(exc_value, SuspiciousOperation):
+                return False
+        return True
+
     @staticmethod
     def _get_attributes(record: logging.LogRecord):
         attributes = LoggingHandler._get_attributes(record)
@@ -23,12 +31,12 @@ class CustomLoggingHandler(LoggingHandler):
                 # Split request into compatible types if present
                 # This occurs when getting "Not Found" error
                 request: HttpRequest = attributes.pop("request")
-                attributes["request.host"] = request.headers.get("Host")
+                attributes["request.host"] = request.headers.get("Host", "")
                 attributes["request.path"] = request.path
-                attributes["request.query_string"] = request.META.get("QUERY_STRING")
+                attributes["request.query_string"] = request.META.get("QUERY_STRING", "")
                 attributes["request.method"] = request.method
-                attributes["request.user_agent"] = request.headers.get("User-Agent")
-                attributes["request.ip"] = request.META.get("X-Forwarded-For", request.META.get("REMOTE_ADDR"))
+                attributes["request.user_agent"] = request.headers.get("User-Agent", "")
+                attributes["request.ip"] = request.META.get("X-Forwarded-For", request.META.get("REMOTE_ADDR", ""))
 
         return attributes
 
