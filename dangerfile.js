c3e81c3565287cbb7d58c4c7226d591f0e42be21
jolpica <47005135+jolpica@users.noreply.github.com>
2024-09-28 16:50:10 +0100

    Update dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -1,16 +1,20 @@
-// danger.js
-
 import { danger, fail, markdown } from "danger";
 
-// Get the pull request body
 const prBody = danger.github.pr.body;
 
-// Check if the required checkbox is checked
-const checkboxRegex = /\* \[x\] I have read and agreed to the contributing guidelines\./i;
+const checkboxRegex = /- \[x\] I have read and agreed to the \[?contributing guidelines\]?/i;
+const unitTestsCheckboxRegex = /- \[x\] Unit tests for the changes are included in this PR\./i;
+
 
 if (!checkboxRegex.test(prBody)) {
-  fail("Please check the 'I have read and agreed to the contributing guidelines.' checkbox in the PR description.");
+  fail("Please confirm you have read and agreed to the contributing guidelines in the PR description.");
   markdown(
-    "🔔 Make sure to check the checkbox to indicate you've read and agreed to the contributing guidelines before submitting your PR. This ensures that all contributors are aware of the standards expected."
+    "🔔 Please check the checkbox in the PR template to indicate you've read and agreed to the contributing guidelines before submitting your PR."
   );
+}
+
+const pythonFiles = danger.git.modified_files.concat(danger.git.created_files).filter(file => file.endsWith(".py"));
+
+if (pythonFiles.length > 0 && !unitTestsCheckboxRegex.test(prBody)) {
+  fail("Please confirm that unit tests for the code changes are included in this PR.");
 }
