031e9fc9e0e8155d84e027cb730eee1e2f1f99c2
jolpica <47005135+jolpica@users.noreply.github.com>
2024-10-02 21:59:38 +0100

    update fixtures

--- scripts/dump_data.sh
+++ scripts/dump_data.sh
@@ -1,7 +1,7 @@
 DATABASE_HOST=$1
 #set PGPASSWORD environment variable
 
+pg_dump -h localhost -U postgres -d jolpica -t formula_one* -a > scripts/dump.sql
 python manage.py migrate formula_one zero
 python manage.py migrate formula_one
-pg_dump -h localhost -U postgres -d jolpica -t formula_one* -a > scripts/dump.sql
 psql -h $DATABASE_HOST -U postgres -d jolpica -f scripts/dump.sql
