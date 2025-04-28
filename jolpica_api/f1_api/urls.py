bea339f303f2750622b8fe989093b40bfd5ddaab
jolpica <47005135@users.noreply.github.com>
2025-04-28 11:58:15 +0000

    Initial schedule

--- jolpica_api/f1_api/urls.py
+++ jolpica_api/f1_api/urls.py
@@ -4,13 +4,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r"seasons", views.SeasonViewSet, basename="season")
-router.register(r"rounds", views.RoundViewSet, basename="round")
-router.register(r"circuits", views.CircuitViewSet, basename="circuit")
-router.register(r"teams", views.TeamViewSet, basename="team")
-router.register(r"drivers", views.DriverViewSet, basename="driver")
-router.register(r"sessions", views.SessionViewSet, basename="session")
-router.register(r"results", views.SessionEntryViewSet, basename="result")
+router.register(r"schedule", views.SeasonScheduleViewSet, basename="season-schedule")
 
 
 urlpatterns = [
