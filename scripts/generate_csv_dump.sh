b78a1e3d68f2cf5db91693cbe67b25a03982f16c
jolpica <47005135@users.noreply.github.com>
2025-05-18 19:55:34 +0000

    Generate dumps without \copy

--- scripts/generate_csv_dump.sh
+++ scripts/generate_csv_dump.sh
@@ -1,19 +1,16 @@
 DATABASE_HOST=${1:-localhost}
+USERNAME=${2:-postgres}
+DATABASE=${3:-jolpica}
 #set PGPASSWORD environment variable
 echo $DATABASE_HOST
 
-tables=$(psql -h $DATABASE_HOST -U postgres -d jolpica -t -c "SELECT tablename FROM pg_tables WHERE tablename LIKE 'formula_one_%';")
+tables=$(psql -h $DATABASE_HOST -U $USERNAME -d $DATABASE -t -c "SELECT tablename FROM pg_tables WHERE tablename LIKE 'formula_one_%';")
 
 rm -rf dump/
 mkdir -p dump
 
 for table in $tables; do
-  psql -h $DATABASE_HOST -U postgres -d jolpica -c "\copy (
-  SELECT *
-  FROM 
-      ${table}
-  ) TO 'dump/${table}.csv'
-  WITH CSV HEADER;"
+  psql -h $DATABASE_HOST -U $USERNAME -d $DATABASE --csv -c "SELECT * FROM ${table};" > "dump/${table}.csv"
   echo "Dumped ${table}.csv"
 done
 
