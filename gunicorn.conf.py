0d33768be3e05f41daf477e194ffb9ba7912a89d
Jessica Perry <47005135+jolpica@users.noreply.github.com>
2026-06-21 20:59:58 +0000

    Add more otel attributes to gunicorn access logs

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -15,6 +15,16 @@ threads = 5
 
 worker_class = "gthread"
 
-access_log_format = "%({x-forwarded-for}i)s %(u)s %(M)s '%(r)s' %(s)s %(b)s %({origin}i)s '%(a)s' ||HOST:%({Host}i)s||"
+access_log_format = (
+    "%({x-forwarded-for}i)s %(u)s %(M)s '%(r)s' %(s)s %(b)s %({origin}i)s '%(a)s'"
+    " ||HOST:%({Host}i)s||"
+    "||PATH:%(U)s||"
+    "||QUERY_STRING:%(q)s||"
+    "||METHOD:%(m)s||"
+    "||USER_AGENT:%(a)s||"
+    "||IP:%({x-forwarded-for}i)s||"
+    "||USER:%(u)s||"
+    "||DURATION:%(M)s||"
+)
 
 logconfig_dict = LOG_CONFIG
